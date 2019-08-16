ec01c05e4720380c7a0f58a068c506e85b868aa8
insideoutzombie <david@web3devs.com>
2019-08-16 15:56:00 -0500

    more functions

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 //
 // put the questions into an array
 //
-// rim the code and should give u an index of the arrays
+// run the code and should give u an index of the arrays
 //
 // q=[q1,q2,q3] array of example q's to print a random question
 // will return a number from 0-3
@@ -23,17 +23,24 @@ console.log("test");
 // * A method to output the question and the available answers.
 // * A method that accepts an integer, and checks if the value matches the ```Correct Answer Index```.
 
+// #### Each answer should have a number displayed that the User can enter to pick an answer
+// (using the Question class method you implemented) (2 points).
+// Refer to image. (HINT: Use 0 - however many answers and use a ```Template String``` or
+// simply concatenate the corresponding answer (+) to make a numbered answer in the console).
+
 class Question {
   constructor(text, answers, correctAI) {
     this.text = text;
     this.answers = answers;
     this.correctAI = correctAI;
   }
-
+  askQuestion() {
+   return ;
+ }
 }
 
 function main() {
-  let arrayOfQuestions = ["q1", "q2", "q3"];
+  let arrayOfQuestions = [];
   let arrayOfObjects = [];
 
   arrayOfQuestions.forEach(
@@ -48,4 +55,29 @@ function main() {
   }
 }
 
+let q1 = new Question('How many million miles is Earth from the Sun?',['123', '93', '150'], 1);
+let index_of_a_question = Math.floor(Math.random() * arrayOfQuestions.length);
+let user;
+
 main();
+debug;
+while(user !== "q") {
+  user = parseInt(prompt("Please make a selection"));
+  switch (user) {
+    case "q":
+      break;
+    case "1":
+      console.log("1000");
+      break;
+    case "2":
+      console.log("2000");
+      break;
+    }
+}
+
+
+//
+// #### Create 3 or more Question instances via the Question class constructor (1 point).
+// Create question instances manually in your JavaScript code.
+// An example of creating a new Question should look something like this
+// (NOTE: Passing in the values for the 3 properties of the Question class to its Constructor):
